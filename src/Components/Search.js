dc1bf045d4637de6efb2839322431aa623cc1273
tranduoc27 <tranduocpytn@gmail.com>
2020-10-02 16:37:16 +0700

    connect component

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -5,7 +5,7 @@ class Search extends Component {
     return (
       <div className="searchForm container">
         <div className="row">
-          <div className="col-12">
+          <div className="col-6">
             <form className="form-inline">
               <div className="form-group mx-sm-3 mb-2">
                 <label htmlFor="search" className="sr-only">
@@ -22,6 +22,28 @@ class Search extends Component {
                 Search
               </button>
             </form>
+          </div>
+          <div className="col-6">
+            <div className="row">
+              <div className="col-6">
+                <button
+                  type="button"
+                  className="btn btn-primary container mb-2"
+                  onClick={() => this.props.connectComponent()}
+                >
+                  Add User
+                </button>
+              </div>
+              <div className="col-6 text-center">
+                <button
+                  type="button"
+                  className="btn btn-danger"
+                  onClick={() => this.props.connectComponent()}
+                >
+                  Close
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
